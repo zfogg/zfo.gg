1b4400a6c510ea3a01c1b35211398770ec1158d7
Zachary Fogg <me@zfo.gg>
2026-03-09 21:15:25 -0400

    add logging for cursor drag deadzone blocks

--- client/src/hooks/useGravity.ts
+++ client/src/hooks/useGravity.ts
@@ -114,6 +114,8 @@ export const useGravity = (externalConfig?: GravityConfig) => {
         normalize(d, r);
         v[0] = -d[0] * g;
         v[1] = -d[1] * g;
+      } else if (r !== 0 && r <= config.distance && b2.mass > 0) {
+        console.log('Cursor drag deadzone blocked:', { r, deadzone: config.distance });
       }
 
       vectors.put(d);
