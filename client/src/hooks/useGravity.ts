0ca33f57de2f720112b2ed75913ef7b462f8da7f
Zachary Fogg <me@zfo.gg>
2026-03-09 21:16:23 -0400

    only log cursor deadzone when b2 is cursor object

--- client/src/hooks/useGravity.ts
+++ client/src/hooks/useGravity.ts
@@ -114,7 +114,7 @@ export const useGravity = (externalConfig?: GravityConfig) => {
         normalize(d, r);
         v[0] = -d[0] * g;
         v[1] = -d[1] * g;
-      } else if (r !== 0 && r <= config.distance && b2.mass > 0) {
+      } else if (r !== 0 && r <= config.distance && b2 === cursor) {
         console.log('Cursor drag deadzone blocked:', { r, deadzone: config.distance });
       }
 
