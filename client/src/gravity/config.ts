291b85ec967ec140c338abadf62083bdae7288a9
Zachary Fogg <me@zfo.gg>
2026-03-12 21:45:34 -0400

    Increase gravity deadzone radius 5x
    Change distance from randomBetween(10, 25) to randomBetween(50, 125)

--- client/src/gravity/config.ts
+++ client/src/gravity/config.ts
@@ -4,7 +4,7 @@ import type { GravityConfig } from "../hooks/useGravity";
 export const getDefaultGravityConfig = (): GravityConfig => ({
   gravity: randomBetween(4, 9) * Math.pow(10, -3) * 2,
   friction: randomBetween(2, 6) * Math.pow(10, -4) * 3,
-  distance: randomBetween(10, 25),
+  distance: randomBetween(50, 125),
   cursorFriction: randomBetween(1, 4),
   cursorMass: 5000,
   cursorForce: 0.65,
