a61408bb618a6d4f8cebcca34c3fa8760090a8eb
Zachary Fogg <me@zfo.gg>
2026-03-26 15:10:18 -0400

    feat(notfound): add GridZips canvas animation

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,23 +1,37 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useGridZips, getDefaultGridZipsConfig } from "../hooks/useGridZips";
 
 const NotFound = () => {
+  const { canvasRef } = useGridZips(getDefaultGridZipsConfig());
+
   // Update page title
   useEffect(() => {
     document.title = "404 - zfo.gg";
   }, []);
 
   return (
-    <section id="content" className="flex-1 flex justify-center items-center w-full px-4">
-      <div className="max-w-4xl mx-auto text-center">
-        <h2>404 :/</h2>
-        <h3>
-          Are you lost?
-          <br />
-          You should <Link to="/">head back</Link> to safety.
-        </h3>
+    <div className="relative flex-1 w-full overflow-hidden">
+      <canvas
+        ref={canvasRef}
+        id="canvas"
+        className="fixed inset-0 w-screen h-screen cursor-crosshair"
+        onContextMenu={(e) => e.preventDefault()}
+      />
+
+      <div className="absolute inset-0 flex flex-col pointer-events-none">
+        <section id="content" className="flex-1 flex justify-center items-center w-full px-4">
+          <div className="max-w-4xl mx-auto text-center pointer-events-auto">
+            <h2>404 :/</h2>
+            <h3>
+              Are you lost?
+              <br />
+              You should <Link to="/">head back</Link> to safety.
+            </h3>
+          </div>
+        </section>
       </div>
-    </section>
+    </div>
   );
 };
 
