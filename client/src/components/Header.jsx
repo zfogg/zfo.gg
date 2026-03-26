12e6f9a0864727d9737e8fd8cdab840bba61d032
Zachary Fogg <me@zfo.gg>
2026-03-26 15:11:17 -0400

    fix(header): add z-index and pointer-events to appear above canvas

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Header = ({ subtitle }) => {
   return (
-    <header id="header" className="text-center pt-8">
+    <header id="header" className="text-center pt-8 relative z-10 pointer-events-auto">
       <Link to="/">
         <h1>zfogg</h1>
       </Link>
-      {subtitle && <p className="text-2xl pointer-events-auto">{subtitle}</p>}
+      {subtitle && <p className="text-2xl">{subtitle}</p>}
     </header>
   );
 };
