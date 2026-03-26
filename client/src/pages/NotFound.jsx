4e4bedf502d099c6a1b40fbc6ceeb041ed6a1a9e
Zachary Fogg <me@zfo.gg>
2026-03-26 14:41:23 -0400

    refactor(notfound): remove inline header, update structure for Layout

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -8,14 +8,7 @@ const NotFound = () => {
   }, []);
 
   return (
-    <div className="flex-1 flex flex-col justify-center items-center w-full">
-      <header id="header" className="text-center">
-        <Link to="/">
-          <h1>zfogg</h1>
-        </Link>
-      </header>
-
-      <section id="content" className="flex-1 flex justify-center items-center w-full px-4">
+    <section id="content" className="flex-1 flex justify-center items-center w-full px-4">
         <div className="max-w-4xl mx-auto text-center">
           <h2>404 :/</h2>
           <h3>
@@ -25,7 +18,6 @@ const NotFound = () => {
           </h3>
         </div>
       </section>
-    </div>
   );
 };
 
